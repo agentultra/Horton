1e52a2f77a5bdbacc327261ac8a168107c420741
James King <james@agentultra.com>
2013-04-26 15:11:55 -0400

    Created package
    Made a bare-bones setup.py script and moved conway.py and grid.py into
    the conway package.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+__author__ = "James King"
+__email__ = "james@agentultra.com"
+__version__ = "0.1.1"
+__license__ = "MIT"
+
+
+setup(
+    name="Conway",
+    version=".".join(__version__),
+    packages=find_packages(),
+
+    author=__author__,
+    author_email=__email__,
+    license=__license__,
+    description="A library for building grid-based simulations.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries",
+    ]
+)
