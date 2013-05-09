d0c721e5fdee50fa3235ef72b1bb8e16bc0e4987
James King <james@agentultra.com>
2013-05-09 10:03:19 -0400

    Added basic controls to the demo
    You can now adjust the grid size and width in the demo using the '+'
    and '-' keys to adjust padding and the arrow keys to adjust the size.
    It's kind of fun to play around with and my rendering calculations do
    some fun things when you get close to 1 on the various axis.

--- horton/examples/pg.py
+++ horton/examples/pg.py
@@ -7,8 +7,17 @@ from horton.grid import Torus
 from horton.render.pg import render_grid
 
 
+SCREEN_W, SCREEN_H = (640, 480)
+GRID_W, GRID_H = (300, 300)
+GRID_PADDING = 4
+FONT_COLOUR = (0, 0, 255)
+
+
 pygame.init()
+pygame.font.init()
+
 screen = pygame.display.set_mode((640, 480))
+font = pygame.font.Font(None, 18)
 
 
 running = 1
@@ -24,6 +33,22 @@ while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = 0
+        elif event.type == KEYDOWN:
+            if event.key == K_EQUALS:
+                GRID_PADDING += 1
+            elif event.key == K_MINUS:
+                if GRID_PADDING > 1:
+                    GRID_PADDING -= 1
+            elif event.key == K_RIGHT:
+                GRID_W += 1
+            elif event.key == K_LEFT:
+                if GRID_W > 1:
+                    GRID_W -= 1
+            elif event.key == K_UP:
+                GRID_H += 1
+            elif event.key == K_DOWN:
+                if GRID_H > 1:
+                    GRID_H -= 1
 
     random_cell = random.choice(world.items())
     if random_cell[1]:
@@ -32,7 +57,17 @@ while running:
         world[random_cell[0]] = 1
 
     screen.fill((255, 255, 255))
-    render_grid(screen, world, 10, 10, 400, 400, padding=2)
+    render_grid(screen, world,
+                (SCREEN_W / 2) - (GRID_W / 2),
+                (SCREEN_H / 2) - (GRID_H / 2),
+                GRID_W, GRID_H,
+                padding=GRID_PADDING)
+    grid_w = font.render("GRID_W: %s" % GRID_W, True, FONT_COLOUR)
+    grid_h = font.render("GRID_H: %s" % GRID_H, True, FONT_COLOUR)
+    grid_pad = font.render("GRID_PADDING: %s" % GRID_PADDING, True, FONT_COLOUR)
+    screen.blit(grid_w, (0, SCREEN_H - 40))
+    screen.blit(grid_h, (0, SCREEN_H - 25))
+    screen.blit(grid_pad, (0, SCREEN_H - 10))
     pygame.display.flip()
 
 
