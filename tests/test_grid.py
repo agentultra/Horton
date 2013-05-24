d7495baa6439050946e0f5e95d7cb0217ebe4b5f
James King <james@agentultra.com>
2013-05-23 23:18:00 -0400

    Added some more tests

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -23,6 +23,8 @@ class TestGrid(unittest.TestCase):
         self.assertEqual(self.g[0, 0], 0)
         g = grid.Grid(3, 8)
         self.assertEqual(g[1, 6], 0)
+        g = grid.Grid(8, 3)
+        self.assertEqual(g[5, 1], 0)
 
     def test_grid_setitem(self):
         self.g[0, 0] = 1
@@ -46,3 +48,35 @@ class TestGrid(unittest.TestCase):
             return True
         else:
             raise AssertionError("Was able to retrieve an invalid location")
+
+    def test_grid_equality(self):
+        g = grid.Grid(5, 5)
+        self.assertEqual(g, self.g)
+
+    def test_grid_of_different_size_is_not_equal(self):
+        g = grid.Grid(3, 3)
+        self.assertNotEqual(g, self.g)
+
+    def test_grid_of_different_value_is_not_equal(self):
+        g = grid.Grid(5, 5)
+        g[0, 0] = 1
+        self.assertNotEqual(g, self.g)
+
+    def test_grid_addition(self):
+        self.g[0, 0] = 1
+        g1 = grid.Grid(5, 5)
+        g1[0, 0] = 1
+        g2 = self.g + g1
+        g3 = grid.Grid(5, 5)
+        g3[0, 0] = 2
+        self.assertEqual(g2, g3)
+
+    def test_coordinates(self):
+        g = grid.Grid(2, 2)
+        self.assertEqual(g.coordinates, [(0, 0), (1, 0), (0, 1), (1, 1)])
+
+    def test_values(self):
+        g = grid.Grid(2, 2)
+        g[0, 0] = 2
+        g[1, 1] = 1
+        self.assertEqual(g.values, [2, 0, 0, 1])
